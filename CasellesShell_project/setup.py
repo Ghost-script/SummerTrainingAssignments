e4c963d1043ff309a593dff3da960fe3fa0fb0ce
Josep Caselles <josepcaselles@gmail.com>
2013-07-24 21:12:45 +0200

    Corrects url in setup.py

--- CasellesShell_project/setup.py
+++ CasellesShell_project/setup.py
@@ -9,12 +9,12 @@ from setuptools import find_packages, setup
 
 setup (
         name = "CasellesShell",
-        version = "1.0.0.a1",
+        version = "1.0.0.a2",
         long_description = "Sample script using cmd2 and requests modules",
         platforms = ["Linux"],
         author = "Arnau Orriols (Josep.Caselles)",
         author_email = "josepcaselles@gmail.com",
-        url = "myshel://github.com/JCaselles/SummerTrainingAssignments/tree/"
+        url = "https://github.com/JCaselles/SummerTrainingAssignments/tree/"
                 "master/CasellesShell_project",
         scripts = ["myshell/CasellesShell"],
         install_requires = ["cmd2", "requests"],
