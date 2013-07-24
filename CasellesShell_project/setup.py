63c9587c9445748cc191cf3f1e5bc4de160a5aa9
Josep Caselles <josepcaselles@gmail.com>
2013-07-24 21:05:25 +0200

    Sets setup.py to alpha release

--- CasellesShell_project/setup.py
+++ CasellesShell_project/setup.py
@@ -9,12 +9,13 @@ from setuptools import find_packages, setup
 
 setup (
         name = "CasellesShell",
-        version = "0.2.3",
+        version = "1.0.0.a1",
         long_description = "Sample script using cmd2 and requests modules",
         platforms = ["Linux"],
         author = "Arnau Orriols (Josep.Caselles)",
         author_email = "josepcaselles@gmail.com",
-        url = "",
+        url = "myshel://github.com/JCaselles/SummerTrainingAssignments/tree/"
+                "master/CasellesShell_project",
         scripts = ["myshell/CasellesShell"],
         install_requires = ["cmd2", "requests"],
         packages = find_packages()
