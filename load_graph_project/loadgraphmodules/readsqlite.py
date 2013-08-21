71cd7a02c88f1368e0be52c62acb18f52d0ef374
Josep Caselles <josepcaselles@gmail.com>
2013-08-21 17:31:38 +0200

    Prettifies readsqlite.py, TODO: comment it out

--- load_graph_project/loadgraphmodules/readsqlite.py
+++ load_graph_project/loadgraphmodules/readsqlite.py
@@ -5,40 +5,51 @@ Reads the database and returns the desired content.
 
 """
 
-from sqlite3 import *
+from sqlite3 import connect
 from calendar import timegm
 from time import strptime
 
+
+
 def fetch_database( select_by, start, end, day_for_time, database_name):
+    """
+    Selects the content from the sqlite database 
+
+
+    """
 
     queries = [[],[],[]]
 
-
     database = connect(database_name)
     cursor = database.cursor()
+    
     if str(select_by) == "all" :
-        print "using tita"
+        cursor.execute("""
+                SELECT date, time, load_1m, load_5m, load_15m
+                FROM load_values""" )
 
-        cursor.execute("""SELECT date, time, load_1m, load_5m, load_15m FROM load_values""" )
     else:
         sqlite_string = """
-            SELECT date, time, load_1m, load_5m, load_15m FROM load_values
-            WHERE %s BETWEEN :start AND :end 
+            SELECT date, time, load_1m, load_5m, load_15m
+            FROM load_values
+            WHERE %s BETWEEN :start AND :end
             """ % select_by
+
         if select_by == "time":
-            sqlite_string += "AND date = :day_for_time" 
-            print sqlite_string
-        cursor.execute(sqlite_string, {"start" : str(start), "end" : str(end), "day_for_time" : str(day_for_time)})
+            sqlite_string += "AND date = :day_for_time"
+
+        cursor.execute(sqlite_string, {"start" : str(start),
+                                       "end" : str(end),
+                                       "day_for_time" : str(day_for_time)})
 
     for row in cursor.fetchall():
-        print row
         time2timestamp = int(timegm(strptime(row[0]+row[1], "%Y%m%d%H%M%S")))
         queries[0].append([time2timestamp, row[2]])
         queries[1].append([time2timestamp, row[3]])
         queries[2].append([time2timestamp, row[4]])
 
-    
     return queries
+
 
 
 if __name__ == "__main__":
